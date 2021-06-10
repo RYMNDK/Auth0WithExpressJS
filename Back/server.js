bd9eb87e5d191c216e56b04b04e1de7a92f50e5a
RYMND <39932526+chrysanthbuster@users.noreply.github.com>
2021-06-10 13:15:48 -0400

    update backend

--- Back/server.js
+++ Back/server.js
@@ -1,21 +1,26 @@
 const express = require("express");
 
-//const cors = require("cors");
+const bodyParser = require('body-parser');
 const morgan = require("morgan");
+const cors = require("cors");
+
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(morgan("common"));
-/* app.use(cors({
-    origin: ["http://localhost:3000"],
-    methods: ["GET", "POST"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-})); */
+//accept only post request from react
+app.use(cors({
+    //origin: ["http://localhost:3000"],
+    methods: ["POST"],
+}));
 
 //app.use(compression());
 //helmet
 
 app.post("/getMessages", (req, res) => {
     console.log(req.body);
+    var messages = [];
     res.json({
         "response" : "this resource"
     });
